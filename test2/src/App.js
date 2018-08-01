ef1785f8b9d021c38296be85e742d4ac89bf7691
114652 <nomo547@hotmail.com>
2018-08-01 15:16:57 +0800

    后台样式测试

--- test2/src/App.js
+++ test2/src/App.js
@@ -1,21 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import './App.css';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+class App extends React.Component {
+  state = {
+    collapsed: false,
+    _width:"200px",
+    iscollapsedWidth:80
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout style={{height:"100vh"}}>
+      <Header>Header</Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+      // <Layout style={{height:"100vh"}}>
+      //   <Sider
+      //     width={this.state._width}
+      //     breakpoint="lg"
+      //     collapsedWidth={this.state.iscollapsedWidth}
+      //     onBreakpoint={(broken) => 
+      //         {
+      //           if(broken){
+      //             this.setState({
+      //               iscollapsedWidth:0,
+      //               _width:"90%"
+      //             })
+      //           }
+      //         }
+      //     }
+      //     trigger={null}
+      //     collapsible
+      //     collapsed={this.state.collapsed}
+      //   >
+      //     <div className="logo" />
+      //     <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+      //       <Menu.Item key="1">
+      //         <Icon type="user" />
+      //         <span>nav 1</span>
+      //       </Menu.Item>
+      //       <Menu.Item key="2">
+      //         <Icon type="video-camera" />
+      //         <span>nav 2</span>
+      //       </Menu.Item>
+      //       <Menu.Item key="3">
+      //         <Icon type="upload" />
+      //         <span>nav 3</span>
+      //       </Menu.Item>
+      //     </Menu>
+      //   </Sider>
+      //   <Layout>
+          
+      //     <Header style={{ background: '#fff', padding: 0 }}>
+      //       <Icon
+      //         className="trigger"
+      //         type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+      //         onClick={this.toggle}
+      //       />
+      //     </Header>
+      //     <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+      //       Content
+      //     </Content>
+      //   </Layout>
+      // </Layout>
     );
   }
 }
+
+
 
 export default App;
